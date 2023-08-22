32f15fc5c27c35fabf44218842327f60ff6eb53a
Gustavo Aguilera <gustavo.urzua2001@gmail.com>
2023-08-22 00:03:59 -0300

    initial commit

---
+++ back/src/test/java/com/vynilStore/vynilStore/VynilStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vynilStore.vynilStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VynilStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
