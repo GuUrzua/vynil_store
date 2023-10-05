0026453fffba390438a972c8ca12eb9f4c24267f
Gustavo Urzua <75682226+GuUrzua@users.noreply.github.com>
2023-10-05 20:58:14 -0300

    Merge branch to develop
    Adding commit to close right issue

--- back/src/main/java/com/vynilStore/vynilStore/DTO/AlbumDTO.java
+++ back/src/main/java/com/vynilStore/vynilStore/DTO/AlbumDTO.java
@@ -1,20 +1,14 @@
 package com.vynilStore.vynilStore.DTO;
 
-import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
-public class AlbumDTO implements Serializable {
+public class AlbumDTO {
     private String name;
-    private MultipartFile albumCover;
     private String artist;
     private LocalDate releaseDate;
-    // private List<String> songs;
 }
--- front/src/app/services/album.service.ts
+++ front/src/app/services/album.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment.dev';
 import { Album } from '../models/Album.model';
@@ -7,19 +7,10 @@ import { Album } from '../models/Album.model';
   providedIn: 'root'
 })
 export class AlbumService {
-    headers = new HttpHeaders({
-        'Content-Type': 'multipart/form-data;boundary=------------------XXXXXXXXXXXX',
-    });
 
   constructor(private http: HttpClient) { }
 
   public createAlbum(album: Album) {
-    const releaseDate = album.releaseDate.toString();
-    const formData = new FormData();
-    formData.append('artist', album.artist);
-    formData.append('name', album.name);
-    formData.append('releaseDate', releaseDate);
-    formData.append('albumCover', album.albumCover);
-    return this.http.post<Album>(`${environment.apiUrl}/album/create`, formData);
+    return this.http.post<Album>(`${environment.apiUrl}/album/create`, album);
   }
 }
