50d65901a279b1b3ea40aa07de413ec7d49225c7
Gustavo Aguilera <gustavo.urzua2001@gmail.com>
2023-10-05 20:50:40 -0300

    feat: adicionando upload de imagem para a cração de Album

--- back/src/main/java/com/vynilStore/vynilStore/DTO/AlbumDTO.java
+++ back/src/main/java/com/vynilStore/vynilStore/DTO/AlbumDTO.java
@@ -1,14 +1,20 @@
 package com.vynilStore.vynilStore.DTO;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
-public class AlbumDTO {
+public class AlbumDTO implements Serializable {
     private String name;
+    private MultipartFile albumCover;
     private String artist;
     private LocalDate releaseDate;
+    // private List<String> songs;
 }
